9a33791fd27fb4bd4783a43c757352e7be2be612
Nael Shiab <nael.shiab@gmail.com>
2024-04-09 19:51:34 -0400

    license

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import {
+  EnvelopeClosedIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+  TwitterLogoIcon,
+} from "@radix-ui/react-icons";
 
 const config: DocsThemeConfig = {
   logo: <span>Code Like a Journalist</span>,
@@ -15,13 +21,34 @@ const config: DocsThemeConfig = {
   },
   footer: {
     text: (
-      <span>
-        MIT {new Date().getFullYear()} ©{" "}
-        <a href="https://github.com/nshiab" target="_blank">
-          Nael Shiab
-        </a>
-        .
-      </span>
+      <div style={{ width: "100%" }}>
+        <p>
+          <a
+            href="https://www.code-like-a-journalist.com/"
+            className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
+            target="_blank"
+          >
+            Code Like a Journalist
+          </a>{" "}
+          © {new Date().getFullYear()} by{" "}
+          <a
+            href="https://github.com/nshiab/code-like-a-journalist"
+            className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
+            target="_blank"
+          >
+            Nael Shiab
+          </a>{" "}
+          is licensed under{" "}
+          <a
+            href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1"
+            target="_blank"
+            className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
+          >
+            Creative Commons Attribution 4.0 International
+          </a>
+          .
+        </p>
+      </div>
     ),
   },
 };
