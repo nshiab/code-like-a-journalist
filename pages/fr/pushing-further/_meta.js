2bc5bab3acab5b1fa48a5c6dabe82332e553e8e9
Nael Shiab <nael.shiab@gmail.com>
2025-01-29 15:26:06 -0500

    New lesson on tasks

--- pages/fr/pushing-further/_meta.js
+++ pages/fr/pushing-further/_meta.js
@@ -3,4 +3,5 @@ export default {
     files: "Écrire et lire des fichiers",
     fetch: "Télécharger des données",
     libraries: "Utiliser des bibliothèques",
+    tasks: "Exécuter des tâches",
 };
