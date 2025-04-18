c45ab75ebfe7a60c63c16e30680f813951b815a6
Nael Shiab <nael.shiab@gmail.com>
2025-04-17 21:00:02 -0400

    Ready for french

--- pages/en/git-and-github/_meta.js
+++ pages/en/git-and-github/_meta.js
@@ -2,5 +2,5 @@ export default {
     "git-basics": "How to use Git?",
     "github-basics": "How to use GitHub?",
     "github-actions": "Web scraping with GitHub actions",
-    "github-pages": "GitHub pages with Svelte",
+    "github-pages": "GitHub Pages with Svelte",
 };
--- pages/fr/git-and-github/_meta.js
+++ pages/fr/git-and-github/_meta.js
@@ -2,4 +2,5 @@ export default {
     "git-basics": "Comment utiliser Git?",
     "github-basics": "Comment utiliser GitHub?",
     "github-actions": "Extraire des données avec GitHub Actions",
+    "github-pages": "GitHub Pages avec Svelte",
 };
