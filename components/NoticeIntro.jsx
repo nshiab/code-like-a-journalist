a45398acf45481bf65a23d1c0f19b3ff2d0a0ad9
nshiab <nael.shiab@gmail.com>
2026-09-09 14:16:26 -0400

    Simplify loops and chapter conclusions and soften notice links

--- components/NoticeIntro.jsx
+++ components/NoticeIntro.jsx
@@ -15,21 +15,21 @@ function pickLanguage(lang) {
                 Vous voulez être prévenu quand de nouvelles leçons sont
                 publiées ? Abonnez-vous à{" "}
                 <a
-                    style={{ textDecoration: "underline", fontWeight: "bold" }}
+                    style={{ textDecoration: "underline" }}
                     href="https://mailchi.mp/0db676437a2f/code-like-a-journalist"
                 >
                     l'infolettre ✉️
                 </a>{" "}
                 et donnez une ⭐ au cours sur{" "}
                 <a
-                    style={{ textDecoration: "underline", fontWeight: "bold" }}
+                    style={{ textDecoration: "underline" }}
                     href="https://github.com/nshiab/code-like-a-journalist"
                 >
                     GitHub
                 </a>{" "}
                 pour me garder motivé !{" "}
                 <a
-                    style={{ textDecoration: "underline", fontWeight: "bold" }}
+                    style={{ textDecoration: "underline" }}
                     href="/fr/contact"
                 >
                     Cliquez ici
@@ -43,21 +43,21 @@ function pickLanguage(lang) {
                 Want to know when new lessons are available? Subscribe to the
                 {" "}
                 <a
-                    style={{ textDecoration: "underline", fontWeight: "bold" }}
+                    style={{ textDecoration: "underline" }}
                     href="https://mailchi.mp/0db676437a2f/code-like-a-journalist"
                 >
                     newsletter ✉️
                 </a>{" "}
                 and give a ⭐ to the{" "}
                 <a
-                    style={{ textDecoration: "underline", fontWeight: "bold" }}
+                    style={{ textDecoration: "underline" }}
                     href="https://github.com/nshiab/code-like-a-journalist"
                 >
                     GitHub repository
                 </a>{" "}
                 to keep me motivated!{" "}
                 <a
-                    style={{ textDecoration: "underline", fontWeight: "bold" }}
+                    style={{ textDecoration: "underline" }}
                     href="/en/contact"
                 >
                     Click here
