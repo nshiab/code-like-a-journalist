18bea534dbc93b27a225b698c014b1c098997b47
nshiab <nael.shiab@gmail.com>
2026-09-14 16:21:54 -0400

    Reorder data projects in sidebar

--- content/en/data-projects/_meta.js
+++ content/en/data-projects/_meta.js
@@ -1,7 +1,7 @@
 export default {
     "saving-account-calculator": "Savings calculator 💸",
     "stock-market-simulator": "Stock market simulator 📈",
-    "stats-can-census": "Wrangling Census data 🇨🇦",
     "one-billion-row-challenge": "One billion row challenge 😱",
+    "stats-can-census": "Wrangling Census data 🇨🇦",
     "web-scraping": "Web scraping 🔍",
 };
