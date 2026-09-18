99f809d110716a2275baa1bcb820884234a6b5ae
nshiab <nael.shiab@gmail.com>
2026-09-18 15:03:24 -0400

    Refresh one billion row challenge

--- content/en/data-projects/_meta.js
+++ content/en/data-projects/_meta.js
@@ -1,7 +1,7 @@
 export default {
     "saving-account-calculator": "Savings calculator 💸",
     "stock-market-simulator": "Stock market simulator 📈",
-    "one-billion-row-challenge": "One billion row challenge 😱",
+    "one-billion-row-challenge": "1 billion row challenge 😱",
     "stats-can-census": "Wrangling Census data 🇨🇦",
     "web-scraping": "Web scraping 🔍",
 };
